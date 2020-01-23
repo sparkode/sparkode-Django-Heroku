6aa538efa485dff20f2c5d83da9404e3edf33b3f
Civan98 <52634607+Civan98@users.noreply.github.com>
2020-01-23 16:04:50 -0600

    static

--- sparkode/settings.py
+++ sparkode/settings.py
@@ -129,14 +129,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, 'static'),
+  #  '/static/',
+
+#]
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    '/static/',
-
-]
+    
 
 MEDIA_URL = '/fotos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fotos')
